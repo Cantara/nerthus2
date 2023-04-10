b67e9882067361e71c1af16cb9f6386e035cb7a9
sindrebrurberg <sindre@brurberg.no>
2023-04-10 17:55:03 +0900

    Adding some simple server cashing

--- aws/serverinfo.go
+++ aws/serverinfo.go
@@ -7,8 +7,10 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	ec2types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	log "github.com/cantara/bragi"
 	"os"
 	"strings"
+	"time"
 )
 
 type Server struct {
@@ -17,7 +19,32 @@ type Server struct {
 	Users []string `json:"users"`
 }
 
-func GetServers() (servers []Server, err error) {
+var servers []Server
+var last time.Time
+
+func GetServers() (serversOut []Server, err error) {
+	if len(servers) > 0 {
+		go func() {
+			if time.Now().Sub(last) < time.Second*15 {
+				log.Info("skipping getting servers, too recently done")
+				return
+			}
+			servs, err := getServers()
+			if err != nil {
+				log.Println("failed getting servers")
+				return
+			}
+			servers = servs
+			last = time.Now()
+		}()
+		return servers, nil
+	}
+
+	servers, err = getServers()
+	return servers, err
+}
+
+func getServers() (servers []Server, err error) {
 	var opts []func(*config.LoadOptions) error
 	if os.Getenv("aws.profile") != "" {
 		opts = append(opts, config.WithSharedConfigProfile(os.Getenv("aws.profile")))
